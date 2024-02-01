d4fb5cbe085b0de545b50927bc676e054f1a90e2
StivinTaesoo <stephentersoo17@gmail.com>
2024-02-01 01:41:36 +0100

    More Editing with the aniamtion

---
+++ script.js
@@ -0,0 +1,21 @@
+const elements = document.querySelectorAll(".fade-in");
+
+const options = {
+    root: null,
+    rootMargin: "0px",
+    threshold: 0.4,
+};
+
+const callbacks = (entries) => {
+    entries.forEach((entry) => {
+        if (entry.isIntersecting) {
+            entry.target.classList.add("active");
+        }
+    });
+};
+
+let observer = new IntersectionObserver(cb, options);
+
+elements.forEach((element) => {
+    observer.observe(element);
+});
